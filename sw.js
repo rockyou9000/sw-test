6c76ef6fd7fbb5169f8c28d4fec1bc054a7e1711
daniel.yp <daniel.yp@alibaba-inc.com>
2018-05-09 16:51:52 +0800

    add image

--- sw.js
+++ sw.js
@@ -12,7 +12,8 @@ self.addEventListener('install', function(event) {
         '/sw-test/star-wars-logo.jpg',
         '/sw-test/gallery/bountyHunters.jpg',
         '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
+        '/sw-test/gallery/snowTroopers.jpg',
+        '/sw-test/gallery/child.jpg'
       ]);
     })
   );
